2eaa0d08894ca8c92f5f4ace3456fe6846bd3fd2
kpestov <kpestov91@gmail.com>
2019-11-06 22:28:38 +0500

    Add post model

--- app/app.py
+++ app/app.py
@@ -2,12 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 from config import Configuration
-from posts.blueprint import posts
-
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
 db = SQLAlchemy(app)
-
-app.register_blueprint(posts, url_prefix='/blog')
