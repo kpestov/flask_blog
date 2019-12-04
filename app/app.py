97dd789609bc77f90e21e5d8a8aaf1488b749b87
kpestov <kpestov91@gmail.com>
2019-12-04 15:44:01 +0500

    Add edit post and admin panel

--- app/app.py
+++ app/app.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
 from config import Configuration
 
 app = Flask(__name__)
@@ -13,3 +16,9 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+# Admin
+
+from models import *
+admin = Admin(app)
+admin.add_view(ModelView(Post, db.session))
+admin.add_view(ModelView(Tag, db.session))
