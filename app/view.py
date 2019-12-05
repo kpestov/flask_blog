53f334899b4cbc52f412c28c2188f6ffcc03a24a
kpestov <kpestov91@gmail.com>
2019-12-05 20:13:45 +0500

    custom 404 and styles

--- app/view.py
+++ app/view.py
@@ -6,3 +6,8 @@ from flask import render_template
 def index():
     n = 'Ivan'
     return render_template('index.html', name=n)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
